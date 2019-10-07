8b1c0820e676b7e735c6004c9621c06be8563639
salo <salo>
2019-10-07 14:34:37 +0400

    end content

--- public/js/script.js
+++ public/js/script.js
@@ -17,5 +17,28 @@ $(document).ready(function () {
       }
     }
   });
+  $('.carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    dots: false,
+    navText: [],
+    responsive:{
+      0:{
+        items:1,
+        
+      },
+      480:{
+        items:2
+      },
+      680:{
+        items:3
+      },
+      1024:{
+        items:4
+
+      }
+    }
+  });
 
 });
